4956b022f76a62091af823cc2ea04ae4dfec9594
Jeany-hlangabeza <jeanyh144@gmail.com>
2024-06-01 23:12:40 +0000

    added api intergration

--- src/Weather.js
+++ src/Weather.js
@@ -1,49 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
+
 export default function Weather() {
-  return (
-    <div className="weather">
-      <form>
+  const [loaded, setLoaded] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState("New York");
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      description: response.data.weather[0].description,
+      date: "Saturday 15:00",
+    });
+    setLoaded(true);
+  }
+
+  function handleSearch(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
+    return (
+      <div className="weather">
+        <form onSubmit={handleSearch}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control search-box"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <button type="submit" className="search-btn">
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul className="date-time">
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control search-box"
-              autoFocus="on"
-            />
+          <div className="col-6">
+            <img src={weatherData.icon} alt={weatherData.description} />
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>{" "}
+            <span className="units">
+              <a href="https://jeany.com">℃</a> |
+              <a href="https://jeany.com">℉</a>
+            </span>
           </div>
-          <div className="col-3">
-            <button type="submit" className="search-btn">
-              Search
-            </button>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h1>New york</h1>
-      <ul className="date-time">
-        <li>Saturday 15:00</li>
-        <li>Sunny</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-            alt="sunny-clouds"
-          />
-          <span className="temperature">27</span>{" "}
-          <span className="units">
-            <a href="https://jeany.com">℃</a> |<a href="https://jeany.com">℉</a>
-          </span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Percipitation: 5%</li>
-            <li>Humidity: 33%</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
